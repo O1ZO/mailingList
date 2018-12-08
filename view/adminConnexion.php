bc73da0ec73070ab31ea98d76ffcde200e4d1afe
O1ZO <adrien.castro@hotmail.fr>
2018-12-08 23:03:26 +0100

    Amélioration visuelle de adminConnexion.php

--- view/adminConnexion.php
+++ view/adminConnexion.php
@@ -4,18 +4,43 @@
 
 <?php ob_start(); ?>
 
-<h1>Espace Administrateur</h1>
 
-<form action="index.php?log=admin" method="post">
-	<fieldset>
-		<legend><h2>Connexion à l'espace d'administration :</h2></legend>
-		<label for="adminLog"><strong>Identifiant : </strong></label>
-		<input type="text" name="adminLog"><br><br>
-		<label for="adminPass"><strong>Mot de passe : </strong></label>
-		<input type="password" name="adminPass"><br><br><br>
-		<input type="submit" name="Connexion">
-	</fieldset>
-</form>
+
+<div class="row">
+	<div class="container">
+		<div class="col col-lg-10">
+			<h1>Espace Administrateur</h1>
+		</div>
+		<div class="col-lg-2">
+			<br><form action="index.php?target=adminConnexion" method="post">
+				<input class="btn btn-primary" type="submit" value="Espace Administrateur">
+			</form>
+		</div>
+	</div>
+</div>
+
+<br>
+
+<div class="row">
+	<div class="container">
+		<section class="col-lg-12">
+			<form class="well" action="index.php?log=admin" method="post">
+				<fieldset>
+					<legend><h2>Connexion à l'espace d'administration :</h2></legend>
+					<div class="form-group">
+						<label for="adminLog"><strong>Identifiant : </strong></label>
+						<input class="form-control" type="text" name="adminLog">	
+					</div>
+					<div class="form-group">
+						<label for="adminPass"><strong>Mot de passe : </strong></label>
+						<input class="form-control" type="password" name="adminPass">	
+					</div>
+				</fieldset><br>
+				<input class="btn btn-primary" type="submit" name="Connexion">
+			</form>
+		</section>
+	</div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
--- view/newsletterSubscription.php
+++ view/newsletterSubscription.php
@@ -16,7 +16,7 @@
 		</div>
 	</div>
 
-		
+	<br>
 	
 	<div class="row">
 		<div class="container">
