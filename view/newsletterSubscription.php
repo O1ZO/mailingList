e4d50d02b408f1e82398b2caf59e6b58a84cc774
O1ZO <adrien.castro@hotmail.fr>
2018-12-08 22:29:55 +0100

    Bootstrap + amélioration de newsletterSubscription.php

--- view/newsletterSubscription.php
+++ view/newsletterSubscription.php
@@ -2,31 +2,68 @@
 
 <?php ob_start(); ?>
 
-	<h1>Abonnement à la newsletter</h2>
-	
-	<form action="index.php?action=subscribe" method="post">
-		<fieldset>
-			<legend><h2>Inscription à la newsletter</h3></legend>
-					<label for="name">Nom : </label>
-					<input type="text" name="name"><br><br>
-					<label for="firstName">Prenom : </label>
-					<input type="text" name="firstName"><br><br>
-					<label for="email">E-mail : </label>
-					<input type="email" name="email"><br><br>
-				<fieldset>
-					<legend><h3>Choisissez la newsletter désirée : </h3></legend>
-					<input type="radio" name="newsletter" value=1>
-					<label for="newsletter">Newsletter 1</label><br>
-					<input type="radio" name="newsletter" value=2>
-					<label for="newsletter">Newsletter 2</label><br>
-					<input type="radio" name="newsletter" value=3>
-					<label for="newsletter">Newsletter 3</label><br>
-				</fieldset><br>
-					<input type="submit" value="Envoyer"></input>
-		</fieldset>
-	</form><br><br>
 
-	<a href="index.php?target=adminConnexion">Espace Administrateur</a>
+	<div class="row">
+		<div class="container">
+			<div class="col col-lg-10">
+				<h1>Abonnement à la newsletter</h1>
+			</div>
+			<div class="col-lg-2">
+				<br><form action="index.php?target=adminConnexion" method="post">
+					<input class="btn btn-primary" type="submit" value="Espace Administrateur">
+				</form>
+			</div>
+		</div>
+	</div>
+
+		
+	
+	<div class="row">
+		<div class="container">
+			<section class="col-lg-12">
+				<form class="well" action="index.php?action=subscribe" method="post">
+				<fieldset>
+					<legend>Inscription à la newsletter</legend>
+						<div class="form-group">
+							<label for="name">Nom : </label>
+							<input class="form-control" type="text" name="name">
+						</div>
+						<div class="form-group">
+							<label for="firstName">Prenom : </label>
+							<input class="form-control" type="text" name="firstName">
+						</div>
+						<div class="form-group">
+							<label for="email">E-mail : </label>
+							<input class="form-control" type="email" name="email">
+						</div>
+						<fieldset>
+							<legend>Choisissez la newsletter désirée : </legend>
+							<div class="radio">
+								<label for="newsletter1" class="radio">
+									<input type="radio" name="newsletter" value=1 id="newsletter1">
+									Newsletter 1
+								</label>
+							</div>
+							<div class="radio">
+								<label for="newsletter2" class="radio">
+									<input type="radio" name="newsletter" value=2 id="newsletter2">	
+									Newsletter 2
+								</label>
+							</div>
+							<div class="radio">
+								<label for="newsletter3" class="radio">
+									<input type="radio" name="newsletter" value=3 id="newsletter3">
+									Newsletter 3
+								</label>				
+							</div>
+						</fieldset><br>
+							<input class="btn btn-primary" type="submit" value="Envoyer"></input>
+				</fieldset>
+			</form>
+			</section>
+		</div>
+	</div>
+	
 
 <?php $content = ob_get_clean(); ?>
 
--- view/template.php
+++ view/template.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link href="public/css/main.css" rel="stylesheet" /> 
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet" /> 
     </head>
         
     <body>
