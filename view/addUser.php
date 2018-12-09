93e27bd2714a1dca9dd99b3ed51e9ea575b55f54
O1ZO <adrien.castro@hotmail.fr>
2018-12-09 13:22:39 +0100

    Amélioration visuelle de addUser.php

--- view/addUser.php
+++ view/addUser.php
@@ -4,8 +4,9 @@
 
 <?php ob_start();?>
 
-<div class="row">
-	<div class="container">
+
+<div class="container">
+	<div class="row">
 		<div class="col col-lg-10">
 			<h1><strong>Espace Administrateur</strong> </h1>
 		</div>
@@ -19,41 +20,79 @@
 
 <br>
 
-<a href="index.php?action=listUsersData">Retour à la liste des utilisateurs</a>
+<div class="container">
+	<a class="btn btn-primary btn-lg" href="index.php?action=listUsersData">Retour à la liste des utilisateurs</a>
+</div><br>
 
-	<div>
-		<form action="index.php?action=add" method="post">
-			<fieldset>
-				<h3>ID : Pas encore attribué</h3>
-				<legend><h2>Ajouter un utilisateur</h2></legend>
-				<label for= "login"><strong>Pseudo : </strong></label>
-				<input type="text" name="login"><br><br>
-				<label for= "name"><strong>Nom : </strong></label>
-				<input type="text" name="name"><br><br>
-				<label for= "firstName"><strong>Prenom : </strong></label>
-				<input type="text" name="firstName"><br><br>
-				<label for= "email"><strong>Email : </strong></label>
-				<input type="text" name="email"><br><br>
-				<label for= "password"><strong>Mot de passe : </strong></label>
-				<input type="text" name="password"><br><br>
+<div class="row">
+	<div class="container">
+		<section class="col-lg-12">
+			<form class="well" action="index.php?action=add" method="post">
 				<fieldset>
-					<legend><h3>Choix de la newsletter : </h3></legend>
-					<input type="radio" name="newsletter" value=1>
-					<label for="newsletter">Newsletter 1</label><br>
-					<input type="radio" name="newsletter" value=2>
-					<label for="newsletter">Newsletter 2</label><br>
-					<input type="radio" name="newsletter" value=3>
-					<label for="newsletter">Newsletter 3</label><br>
-				</fieldset><br>
-				<label for= "adminRights"><strong>Administrateur : </strong></label>
-				<input type="radio" name="adminRights" value="oui">Oui</input>
-				<input type="radio" name="adminRights" value="non" checked>Non</input><br><br>
-				<input type="submit" value="Confirmer les changements"></input>
-			</fieldset>
-		</form>
+					<h3><strong>ID : Pas encore attribué</strong></h3><br>
+					<legend>Ajouter un utilisateur</legend>
+					<div class="form-group">
+						<label for= "login"><strong>Pseudo : </strong></label>
+						<input class="form-control" type="text" name="login">
+					</div>
+					<div class="form-group">
+						<label for= "name"><strong>Nom : </strong></label>
+						<input class="form-control" type="text" name="name">
+					</div>
+					<div class="form-group">
+						<label for= "firstName"><strong>Prenom : </strong></label>
+						<input class="form-control" type="text" name="firstName">
+					</div>
+					<div class="form-group">
+						<label for= "email"><strong>Email : </strong></label>
+						<input class="form-control" type="text" name="email">
+					</div>
+					<div class="form-group">
+						<label for= "password"><strong>Mot de passe : </strong></label>
+						<input class="form-control" type="text" name="password">
+					</div>
+					<fieldset>
+						<legend>Choix de la newsletter : </legend>
+						<div class="radio">
+							<label for="newsletter">
+								<input type="radio" name="newsletter" value=1>
+								Newsletter 1
+							</label>
+						</div>
+						<div class="radio">
+							<label for="newsletter">
+								<input type="radio" name="newsletter" value=2>
+								Newsletter 2
+							</label>
+						</div>
+						<div class="radio">
+							<label for="newsletter">
+								<input type="radio" name="newsletter" value=3>
+								Newsletter 3
+							</label>
+						</div>
+					</fieldset>
+					<legend>Administrateur : </legend>
+					<div class="radio">
+						<label for="adminRights">
+							<input type="radio" name="adminRights" value="oui">
+							Oui
+						</label>
+					</div>
+					<div class="radio">
+						<label for="adminRights">
+							<input type="radio" name="adminRights" value="non" checked>
+							Non
+						</label>
+					</div><br>
+					<input class="btn btn-primary" type="submit" value="Ajouter un utilisateur"></input>
+				</fieldset>
+			</form>
+		</section>
 	</div>
+</div>
 
-	
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require ('template.php'); ?>
