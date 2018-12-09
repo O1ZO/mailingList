a32371acde759cd8c9daf9286071e0ebda12e014
O1ZO <adrien.castro@hotmail.fr>
2018-12-09 13:36:13 +0100

    Amélioration visuelle de modifyNewsletter.php

--- view/modifyNewsletter.php
+++ view/modifyNewsletter.php
@@ -4,8 +4,9 @@
 
 <?php ob_start();?>
 
-<div class="row">
-	<div class="container">
+
+<div class="container">
+	<div class="row">
 		<div class="col col-lg-10">
 			<h1><strong>Espace Administrateur</strong> </h1>
 		</div>
@@ -19,24 +20,36 @@
 
 <br>
 
-<a href="index.php?action=listNewsletters">Retour à la liste des newsletter</a>
+<div class="container">
+	<a class="btn btn-primary btn-lg" href="index.php?action=listNewsletters">Retour à la liste des newsletter</a>
+</div><br>
 
-	<div>
-		<form action="index.php?action=updateNl&amp;id=<?= $getNewsletter['id'] ?>" method="post">
-			<fieldset>
-				<legend><h2>Modifier le contenu d'une newsletter</h2></legend>
-				<label for= "newTitle"><strong>Nouveau titre : </strong></label>
-				<input type="text" name="newTitle" value="<?= htmlspecialchars($getNewsletter['title']) ?>"><br><br>
-				<label for= "newSubject"><strong>Nouveau sujet : </strong></label>
-				<input type="text" name="newSubject" value="<?= htmlspecialchars($getNewsletter['subject']) ?>"><br><br>
-				<label for= "newContent"><strong>Nouveau contenu : </strong></label>
-				<textarea name="newContent" rows="15" cols="80"><?= htmlspecialchars($getNewsletter['content']) ?></textarea><br><br>
-				<input type="submit" value="Confirmer les changements"></input>
-			</fieldset>
-		</form>
+<div class="row">
+	<div class="container">
+		<section class="col-lg-12">
+			<form class="well" action="index.php?action=updateNl&amp;id=<?= $getNewsletter['id'] ?>" method="post">
+				<fieldset>
+					<legend>Modifier le contenu d'une newsletter</legend>
+					<div class="form-group">
+						<label for= "newTitle"><strong>Nouveau titre : </strong></label>
+						<input class="form-control" type="text" name="newTitle" value="<?= htmlspecialchars($getNewsletter['title']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newSubject"><strong>Nouveau sujet : </strong></label>
+						<input class="form-control" type="text" name="newSubject" value="<?= htmlspecialchars($getNewsletter['subject']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newContent"><strong>Nouveau contenu : </strong></label>
+						<textarea class="form-control" name="newContent" rows="15" cols="80"><?= htmlspecialchars($getNewsletter['content']) ?></textarea>
+					</div><br>
+					<input class="btn btn-primary" type="submit" value="Confirmer les changements"></input>
+				</fieldset>
+			</form>
+		</section>
 	</div>
+</div>
 
-	
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require ('template.php'); ?>
