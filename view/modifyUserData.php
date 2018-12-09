ed87e7af780e1632420568835c6c4a1a63094a27
O1ZO <adrien.castro@hotmail.fr>
2018-12-09 13:03:10 +0100

    Amélioration visuelle de mofyUserData.php

--- view/modifyUserData.php
+++ view/modifyUserData.php
@@ -3,8 +3,9 @@
 
 <?php ob_start();?>
 
-<div class="row">
-	<div class="container">
+
+<div class="container">
+	<div class="row">
 		<div class="col col-lg-10">
 			<h1><strong>Espace Administrateur</strong> </h1>
 		</div>
@@ -17,51 +18,91 @@
 </div>
 
 <br>
-<div class="container">
-<?php
-if (isset($_GET['action']) && $_GET['action'] == 'modify' || $_GET['action'] == 'update') {
-	echo '<a class=" btn btn-primary btn-lg" href="index.php?action=listUsersData">Retour à la liste des utilisateurs</a>';
-}
-elseif (isset($_GET['nlId']) &&  $_GET['nlId'] > 0 && isset($_GET['action']) && $_GET['action'] == 'modifyFromNl' || $_GET['action'] == 'updateFromNl') {?>
-	<a class=" btn btn-primary btn-lg" href="index.php?action=listSubscribers&amp;nlId=<?= $_GET['nlId'] ?>">Retour à la liste des abonnés</a>
-<?php
-}
-?>
-</div>
 
-	<div>
-		<form action="<?php if(isset($_GET['action']) && $_GET['action'] == 'modify'){?>index.php?action=update&amp;id=<?= $user_data['id'];}elseif(isset($_GET['action']) && $_GET['action'] == 'modifyFromNl'){?>index.php?action=updateFromNl&amp;id=<?= $user_data['id']?><?php if(isset($_GET['nlId']) && $_GET['nlId'] > 0){?>&amp;nlId=<?= $_GET['nlId']	;};}?>" method="post">
-			<fieldset>
-				<h3>ID : <?= $user_data['id'] ?></h3>
-				<legend><h2>Modifier les données des utilisateurs</h2></legend>
-				<label for= "newLogin"><strong>Nouveau pseudo : </strong></label>
-				<input type="text" name="newLogin" value="<?= htmlspecialchars($user_data['login']) ?>"><br><br>
-				<label for= "newName"><strong>Nouveau nom : </strong></label>
-				<input type="text" name="newName" value="<?= htmlspecialchars($user_data['name']) ?>"><br><br>
-				<label for= "newFirstName"><strong>Nouveau prenom : </strong></label>
-				<input type="text" name="newFirstName" value="<?= htmlspecialchars($user_data['firstName']) ?>"><br><br>
-				<label for= "newEmail"><strong>Nouvel email : </strong></label>
-				<input type="text" name="newEmail" value="<?= htmlspecialchars($user_data['email']) ?>"><br><br>
-				<label for= "newPassword"><strong>Nouveau mot de passe : </strong></label>
-				<input type="text" name="newPassword" value="<?= htmlspecialchars($user_data['password']) ?>"><br><br>
+
+<div class="container">
+	<?php
+	if (isset($_GET['action']) && $_GET['action'] == 'modify' || $_GET['action'] == 'update') {
+		echo '<a class=" btn btn-primary btn-lg" href="index.php?action=listUsersData">Retour à la liste des utilisateurs</a>';
+	}
+	elseif (isset($_GET['nlId']) &&  $_GET['nlId'] > 0 && isset($_GET['action']) && $_GET['action'] == 'modifyFromNl' || $_GET['action'] == 'updateFromNl') {?>
+		<a class=" btn btn-primary btn-lg" href="index.php?action=listSubscribers&amp;nlId=<?= $_GET['nlId'] ?>">Retour à la liste des abonnés</a>
+		<?php
+	}
+	?>
+</div><br>
+
+<div class="row">
+	<div class="container">
+		<section class="col-lg-12">
+			<form class= "well" action="<?php if(isset($_GET['action']) && $_GET['action'] == 'modify'){?>index.php?action=update&amp;id=<?= $user_data['id'];}elseif(isset($_GET['action']) && $_GET['action'] == 'modifyFromNl'){?>index.php?action=updateFromNl&amp;id=<?= $user_data['id']?><?php if(isset($_GET['nlId']) && $_GET['nlId'] > 0){?>&amp;nlId=<?= $_GET['nlId']	;};}?>" method="post">
 				<fieldset>
-					<legend><h3>Abonné à : </h3></legend>
-					<input type="radio" name="newNewsletterId" value=1 <?php if($user_data['newsletterId'] == 1){echo 'checked';} ?> >
-					<label for="newNewsletterId">Newsletter 1</label><br>
-					<input type="radio" name="newNewsletterId" value=2 <?php if($user_data['newsletterId'] == 2){echo 'checked';} ?> >
-					<label for="newNewsletterId">Newsletter 2</label><br>
-					<input type="radio" name="newNewsletterId" value=3 <?php if($user_data['newsletterId'] == 3){echo 'checked';} ?> >
-					<label for="newNewsletterId">Newsletter 3</label><br>
-				</fieldset><br><br>
-				<label for= "adminRights"><strong>Administrateur : </strong></label>
-				<input type="radio" name="adminRights" value="oui" <?php if($user_data['adminRights'] == 'oui'){echo 'checked';} ?> >Oui</input>
-				<input type="radio" name="adminRights" value="non" <?php if($user_data['adminRights'] == 'non'){echo 'checked';} ?> >Non</input><br><br>
-				<input type="submit" value="Confirmer les changements"></input>
-			</fieldset>
-		</form>
+					<h3><strong>ID : <?= $user_data['id'] ?></strong></h3><br>
+					<legend>Modifier les données des utilisateurs</legend>
+					<div class="form-group">
+						<label for= "newLogin">Nouveau pseudo : </label>
+						<input class="form-control" type="text" name="newLogin" value="<?= htmlspecialchars($user_data['login']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newName">Nouveau nom : </label>
+						<input class="form-control" type="text" name="newName" value="<?= htmlspecialchars($user_data['name']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newFirstName">Nouveau prenom : </label>
+						<input class="form-control" type="text" name="newFirstName" value="<?= htmlspecialchars($user_data['firstName']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newEmail">Nouvel email : </label>
+						<input class="form-control" type="text" name="newEmail" value="<?= htmlspecialchars($user_data['email']) ?>">
+					</div>
+					<div class="form-group">
+						<label for= "newPassword">Nouveau mot de passe : </label>
+						<input class="form-control" type="text" name="newPassword" value="<?= htmlspecialchars($user_data['password']) ?>">
+					</div>
+					<fieldset>
+						<legend>Abonné à : </legend>
+						<div class="radio">
+							<label for="newNewsletterId">
+								<input type="radio" name="newNewsletterId" value=1 <?php if($user_data['newsletterId'] == 1){echo 'checked';} ?> >
+								Newsletter 1
+							</label>
+						</div>
+						<div class="radio">
+							<label for="newNewsletterId">
+								<input type="radio" name="newNewsletterId" value=2 <?php if($user_data['newsletterId'] == 2){echo 'checked';} ?> >
+								Newsletter 2
+							</label>
+						</div>
+						<div class="radio">
+							<label for="newNewsletterId">
+								<input type="radio" name="newNewsletterId" value=3 <?php if($user_data['newsletterId'] == 3){echo 'checked';} ?> >
+								Newsletter 3
+							</label>
+						</div>
+					</fieldset>
+						<fieldset>
+							<legend>Administrateur : </legend>
+							<div class="radio">
+								<label for="adminRights">
+									<input type="radio" name="adminRights" value="oui" <?php if($user_data['adminRights'] == 'oui'){echo 'checked';} ?> >
+									Oui
+								</label>
+							</div>
+							<div class="radio">
+								<label for="adminRights">
+									<input type="radio" name="adminRights" value="non" <?php if($user_data['adminRights'] == 'non'){echo 'checked';} ?> >
+									Non
+								</label>
+							</div>
+						</fieldset><br>
+						<input class="btn btn-primary" type="submit" value="Confirmer les changements"></input>
+					</fieldset>
+				</form>
+			</section>
+		</div>
 	</div>
 
 	
-<?php $content = ob_get_clean(); ?>
+	<?php $content = ob_get_clean(); ?>
 
-<?php require ('template.php'); ?>
+	<?php require ('template.php'); ?>
